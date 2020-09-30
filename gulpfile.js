cfc76b1e713ff31a7388a3c7a1bb2abdab9a10de
UchihaYuki <uchiha_yuki@live.com>
2020-09-30 11:17:03 +0800

    undefined

--- gulpfile.js
+++ gulpfile.js
@@ -6,10 +6,13 @@ const del = require('del');
 const shell = require('shelljs');
 const sourcemaps = require('gulp-sourcemaps')
 const argv = require('yargs').argv
+const fs = require('fs')
 
 const src = ts.createProject("src/tsconfig.json");
 const srcRelease = ts.createProject("src/tsconfig-release.json");
 const test = ts.createProject("test/tsconfig.json");
+
+const starter = "node-bin-starter";
 
 gulp.task("clean", () => del([
     "dist",
@@ -65,12 +68,44 @@ gulp.task("test", gulp.series("clean", "build:src", "build:test", "run:test"));
 gulp.task("bin", gulp.series("test", "build:src-release", "pkg", 'postpkg'))
 
 gulp.task('push', function (cb) {
-    if (!argv.m) {
-        return cb(new Error("lack commit message: gulp push -m <msg>"))
+    // if (!argv.m) {
+    //     return cb(new Error("usage: gulp push -m <message>"))
+    // }
+    const message = argv.m || new Date().toISOString()
+
+    let index = __dirname.lastIndexOf("/");
+    if (index == -1) index = __dirname.lastIndexOf("\\");
+    const parentDir = __dirname.slice(index + 1);
+    if (parentDir != starter && !fs.existsSync('.init')) {
+        return cb(new Error("project has not been initialized, run 'gulp init' to initialize project"))
     }
 
     shell.exec(`git add .`)
     shell.exec(`git commit -m ${argv.m}`)
     shell.exec(`git push`)
+    cb();
+})
+
+gulp.task('init', function (cb) {
+    if (!argv.r) {
+        return cb(new Error("usage: gulp init -r <repo>"))
+    }
+    
+    let index = __dirname.lastIndexOf("/");
+    if (index == -1) index = __dirname.lastIndexOf("\\");
+    const parentDir = __dirname.slice(index + 1);
+    if (parentDir == starter) {
+        return cb(new Error(`${starter} doesn't need to be initialized`))        
+    }
+
+    if (fs.existsSync('.init')) {
+        return cb(new Error("project has already been initialized"))
+    }
+
+    shell.exec(`git remote set-url origin ${argv.r}`);
+    shell.exec(`touch .init`)
+    shell.exec(`git add .`)
+    shell.exec(`git commit -m init`)
+    shell.exec(`git push -u origin master`)
     cb();
 })
